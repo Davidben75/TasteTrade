0101eaca163c105083fb684dfb2c232be8d25fc9
David <david.benavides@3wa.io>
2023-11-15 09:24:41 +0100

    Navbar Component
    Add a responsive navbar

---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = ()  => {
+    const [active, setActive] = useState("nav-menu");
+    const [icon, setIcon] = useState("nav-toggler");
+    const navToggle = () => {
+      if (active === "nav-menu") {
+        setActive("nav-menu nav-active");
+      } else setActive("nav-menu");
+  
+      // Icon Toggler
+      if (icon === "nav-toggler") {
+        setIcon("nav-toggler toggle");
+      } else setIcon("nav-toggler");
+    };
+    return (
+      <nav className="nav">
+        <span className="logo-container">
+            <NavLink to={"/"} className="nav-brand logo">
+                Taste Tadre
+            </NavLink>
+        </span>
+        
+        <ul className={active}>
+          <li className="nav-item">
+            <NavLink href="#" className="nav-link">
+              Accueil
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink href="#" className="nav-link">
+                à propos
+            </NavLink>
+          </li>
+          <li className="nav-item login-btn">
+            <NavLink href="#" className="nav-link">
+              Se connecter
+            </NavLink>
+          </li>
+          <li className="nav-item signup-btn">
+            <NavLink href="#" className="nav-link ">
+              S'inscrire
+            </NavLink>
+          </li>
+          
+        </ul>
+        <div onClick={navToggle} className={icon}>
+          <div className="line1"></div>
+          <div className="line2"></div>
+          <div className="line3"></div>
+        </div>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
--- front/src/index.js
+++ front/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
     <Header />
+    <Navbar /> 
       <App />
     </BrowserRouter>  
   </React.StrictMode>
