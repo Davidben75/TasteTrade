3c1541ef1ce901f6d65bfc1aa79e3a2c681e365c
david <david>
2024-04-27 14:48:25 +0200

    update .env

--- back/config/database.js
+++ back/config/database.js
@@ -3,14 +3,8 @@ import mongoose from "mongoose";
 
 dotenv.config()
 
-const connectDB = mongoose.connect(process.env.DB_URI);
-
-mongoose.connection.on("open", () => {
-    console.log("Connexion avec la BDD effectuée")
-});
-
-mongoose.connection.on("error", () => {
-    console.log("Connexion impossible à la BDD")
-});
+const connectDB = mongoose.connect(process.env.DB_URI)
+    .then(() => console.log(`Connect to your database`))
+    .catch((error) => console.log(error));
 
 export default connectDB
